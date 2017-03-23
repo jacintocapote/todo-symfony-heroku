c82028bfb4aab21f7a6139419d7de84be6c06dc9
Jacinto capote Robles <jacintocapote@gmail.com>
2017-03-23 14:40:57 +0100

    Added source on the template and remove html tags before insert database

--- src/JCapote/ToDoBundle/Controller/DefaultController.php
+++ src/JCapote/ToDoBundle/Controller/DefaultController.php
@@ -12,20 +12,28 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
+    /**
+     * Call routing for the homepage. Here will return a list with items
+     * and little form on the top to create new items.
+     */
     public function indexAction(Request $request)
     {
+        //Get current items.
         $item_list = $this->getAllToDoItems();
 
         $item = new Item;
       
+        //Form for create new items.
         $form = $this->createFormBuilder($item)
         ->add('text', TextareaType::class, array('label' => 'Insert your text', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
         ->add('Save', SubmitType::class, array('label'=> 'Add Item', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
         ->getForm();
         
         $form->handleRequest($request);
+
+        //Was submitted?. Then process.
         if($form->isSubmitted() &&  $form->isValid()){
-            $text = $form['text']->getData();
+            $text = strip_tags($form['text']->getData());
             
             $item->setText($text);          
             $item->setStatus(FALSE);                  
@@ -33,21 +41,28 @@ class DefaultController extends Controller
             $sn = $this->getDoctrine()->getManager();      
             $sn->persist($item);
             $sn->flush();
-            
+
+            //Show a information message.            
             $this->addFlash(
                 'notice',
                 'Item was successfull added'
             );
+
+            //Reload page.
             return new RedirectResponse($this->generateUrl('homepage'));
         }
  
-
+        //Return output html.
         return $this->render('JcapoteToDoBundle:Default:index.html.twig', array(
             'item_list' => $item_list,
             'form' => $form->createView(),
         ));
     }
 
+    /**
+     * Call routing for complete action. Isn't a page. Is a simple callback to change status 
+     * to completed.
+     */
     public function completeAction(Request $request, $id) {
         $sn = $this->getDoctrine()->getManager();
         $item = $sn->getRepository('JCapote\ToDoBundle\Entity\Item')->find($id);
@@ -56,7 +71,7 @@ class DefaultController extends Controller
             $item->setStatus(TRUE);
             $sn->flush();
             
-
+            //Show a information message.
             $this->addFlash(
                 'notice',
                 'Item was changed to completed'
@@ -65,6 +80,10 @@ class DefaultController extends Controller
         return new RedirectResponse($this->generateUrl('homepage'));
     }
 
+    /**
+     * Call routing for uncomplete action. Isn't a page. Is a simple callback to change status 
+     * to uncompleted.
+     */
     public function uncompleteAction(Request $request, $id) {
         $sn = $this->getDoctrine()->getManager();
         $item = $sn->getRepository('JCapote\ToDoBundle\Entity\Item')->find($id);
@@ -82,6 +101,10 @@ class DefaultController extends Controller
         return new RedirectResponse($this->generateUrl('homepage'));
     }
 
+    /**
+     * Call routing for delete action. Isn't a page. Is a simple callback to delete a item
+     * without confirmation.
+     */
     public function deleteAction(Request $request, $id) {
         $sn = $this->getDoctrine()->getManager();
         $item = $sn->getRepository('JCapote\ToDoBundle\Entity\Item')->find($id);
@@ -90,7 +113,7 @@ class DefaultController extends Controller
             $sn->remove($item);
             $sn->flush();
 
-
+            //Show a information message.
             $this->addFlash(
                 'notice',
                 'Item was removed'
@@ -99,24 +122,32 @@ class DefaultController extends Controller
         return new RedirectResponse($this->generateUrl('homepage'));
     }
 
+    /**
+     * Call routing for edit action. Isn't a page. Is a simple page with a form 
+     * to allow alter item text.
+     */
     public function editAction(Request $request, $id) {
         $sn = $this->getDoctrine()->getManager();
         $item = $sn->getRepository('JCapote\ToDoBundle\Entity\Item')->find($id);
 
         if ($item) {
+            //We create form with default values.
             $form = $this->createFormBuilder($item)
             ->add('text', TextareaType::class, array('label' => 'Insert your text', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
             ->add('Savie', SubmitType::class, array('label'=> 'Save Item', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
             ->getForm();
 
             $form->handleRequest($request);
+
+            //If form was submitted then process.
             if($form->isSubmitted() &&  $form->isValid()) {
-                $text = $form['text']->getData();
+                $text = strip_tags($form['text']->getData());
                 $item = $sn->getRepository('JCapote\ToDoBundle\Entity\Item')->find($id);
 
                 $item->setText($text);
                 $sn->flush();
 
+                //Show a information message.
                 $this->addFlash(
                     'notice',
                     'Item was edited'
@@ -132,6 +163,9 @@ class DefaultController extends Controller
         return new RedirectResponse($this->generateUrl('homepage'));
     }
 
+    /**
+     * API function for get all items.
+     */
     private function getAllToDoItems()
     {
         $items = $this->getDoctrine()
