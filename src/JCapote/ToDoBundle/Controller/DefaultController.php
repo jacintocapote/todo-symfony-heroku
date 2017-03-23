d7c7f35201805fa440ec900bbd2ace57da575117
Jacinto capote Robles <jacintocapote@gmail.com>
2017-03-23 12:51:50 +0100

    Cleaned some html tags and created routing functions for complete/uncomplete and delete

--- src/JCapote/ToDoBundle/Controller/DefaultController.php
+++ src/JCapote/ToDoBundle/Controller/DefaultController.php
@@ -4,48 +4,90 @@ namespace JCapote\ToDoBundle\Controller;
 
 use JCapote\ToDoBundle\Entity\Item;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $item_list = $this->getAllToDoItems();
 
         $item = new Item;
       
         $form = $this->createFormBuilder($item)
-        ->add('text', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('text', TextareaType::class, array('label' => 'Insert your text', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
         ->add('Save', SubmitType::class, array('label'=> 'Add Item', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
         ->getForm();
         
-        /*$form->handleRequest($request);
+        $form->handleRequest($request);
         if($form->isSubmitted() &&  $form->isValid()){
-            $id = 3;
             $text = $form['text']->getData();
             
-            $item->setId($name);
             $item->setText($text);          
             $item->setStatus(FALSE);                  
             
             $sn = $this->getDoctrine()->getManager();      
-            $sn -> persist($item);
-            $sn -> flush();
+            $sn->persist($item);
+            $sn->flush();
             
             $this->addFlash(
                 'notice',
-                'Item Added'
+                'Item was successfull added'
             );
-            return $this->redirectToRoute('todo_list');
-        }*/
+            return new RedirectResponse($this->generateUrl('homepage'));
+        }
  
 
         return $this->render('JcapoteToDoBundle:Default:index.html.twig', array(
             'item_list' => $item_list,
             'form' => $form->createView(),
         ));
+    }
+
+    public function completeAction(Request $request, $id) {
+        $sn = $this->getDoctrine()->getManager();
+        $item = $sn->getRepository('JCapote\ToDoBundle\Entity\Item')->find($id);
+        $item->setStatus(TRUE);
+        $sn->flush();
+            
+
+        $this->addFlash(
+            'notice',
+            'Item was changed to completed'
+        );
+        return new RedirectResponse($this->generateUrl('homepage'));
+    }
+
+    public function uncompleteAction(Request $request, $id) {
+        $sn = $this->getDoctrine()->getManager();
+        $item = $sn->getRepository('JCapote\ToDoBundle\Entity\Item')->find($id);
+        $item->setStatus(FALSE);
+        $sn->flush();
+
+
+        $this->addFlash(
+            'notice',
+            'Item was changed to uncompleted'
+        );
+        return new RedirectResponse($this->generateUrl('homepage'));
+    }
+
+    public function deleteAction(Request $request, $id) {
+        $sn = $this->getDoctrine()->getManager();
+        $item = $sn->getRepository('JCapote\ToDoBundle\Entity\Item')->find($id);
+        $sn->remove($item);
+        $sn->flush();
+
+
+        $this->addFlash(
+            'notice',
+            'Item was removed'
+        );
+        return new RedirectResponse($this->generateUrl('homepage'));
     }
 
     private function getAllToDoItems()
